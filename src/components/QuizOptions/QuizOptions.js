e8212ba22b213a493d07a89945c0ec947d1d1852
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-12-02 14:19:20 +0600

    installed react toast hot and react icons

---
+++ src/components/QuizOptions/QuizOptions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizOptions = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default QuizOptions;
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const QuizQuestions = () => {
-    const [selected, setSelected] = useState('yes');
+    const [selected, setSelected] = useState('');
 
     const quiz = useLoaderData();
 
     console.log(quiz);
     const handleChange = event => {
         console.log(event.target.value);
+
         setSelected(event.target.value);
+
     };
 
 
@@ -26,8 +29,11 @@ const QuizQuestions = () => {
                 {
                     quiz.data.questions.map((question, i) => <div
                         id={question.id}>
-                        <h3 className='text-lg font-bold p-5'>Quiz {i + 1}.
-                            {question.question}</h3>
+                        <div className='flex justify-around'>
+                            <h3 className='text-lg font-bold p-5'>Quiz {i + 1}.<span className='text-violet-200'>..</span>
+                                {question.question.slice(3, -4)}</h3>
+                            <p>{question.correctAnswer}</p>
+                        </div>
                         <div className='md:mx-8 xl:mx-72'>
                             <div className='justify-items-center grid grid-cols-1 md:grid-cols-2'>
                                 {question.options.map(option => <div className='mt-5 bg-orange-50 w-80 h-24 border rounded p-5'><input onChange={handleChange}
